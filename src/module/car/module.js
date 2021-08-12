391e65f0db0e882dfc50d00a00c264fa87cf3d02
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-08-12 16:02:58 -0300

    ADD: Se creo el menú modulo car
    Se crea un menú de lo que el modulo car tiene para ofrecer a los demás módulos

---
+++ src/module/car/module.js
@@ -0,0 +1,11 @@
+const CarController = require('./controller/carController');
+const CarService = require('./service/carService');
+const CarRepository = require('./repository/carRepository');
+const CarModel = require('./model/carModel');
+
+module.exports = {
+  CarController,
+  CarService,
+  CarRepository,
+  CarModel,
+};
