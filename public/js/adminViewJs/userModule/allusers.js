c6a7f42b82d1d40be9c13ac3c3bd318ab02be8b9
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-08-31 14:14:22 -0300

    Se crea una carpeta nueva para los scripts de las views
    Esta carpeta se crea para separar por modulos los scripts que corresponden a los mismos

---
+++ public/js/adminViewJs/userModule/allusers.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-alert */
+document.querySelectorAll('.action-edit-user').forEach(($actionEdit) => {
+  $actionEdit.addEventListener('click', (e) => {
+    const { id, fullname } = $actionEdit.dataset;
+    if (
+      !confirm(
+        `Confirm that you want to edit the User: ${fullname} with ID ${id}? This operation cannot be undone`,
+      )
+    ) {
+      e.preventDefault();
+      return false;
+    }
+    return true;
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    const $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
